29d2c041daddf639d9c71c184c31bf8cd92a8d71
lasvegas2024 <147817941+lasvegas2024@users.noreply.github.com>
2023-11-25 08:53:17 +0800

    Redis 整合11250851

---
+++ src/test/java/com/trus/regpro/RegProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trus.regpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
